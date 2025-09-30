12c2fefe4771d33e41f1cc32167671a19f029310
Vlad <vk@avilo.no>
2025-09-30 22:30:38 +0200

    add Select.tsx

--- components/UI/Button.tsx
+++ components/UI/Button.tsx
@@ -15,7 +15,8 @@ const Button: React.FC<ButtonProps> = ({
     return (
         <button
             type={type}
-            className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 disabled:opacity-50"
+            className="px-4 py-2 bg-blue-500 text-white rounded
+            hover:bg-blue-600 disabled:opacity-50 disabled:cursor-not-allowed"
             onClick={onClick}
             disabled={disabled}
         >
